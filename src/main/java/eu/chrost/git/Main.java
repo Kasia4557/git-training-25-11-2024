522968bf3f2be6106f9de3211c87968958579cbf
labuser <katarzyna.kozak4@o2.pl>
2024-11-29 09:48:18 +0100

    test1

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello World!");
-        System.out.println("hello World World!!!");
+        System.out.println("hello World Wo!!!");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         scanner.close();
