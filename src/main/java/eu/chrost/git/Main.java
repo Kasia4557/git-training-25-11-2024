3baad23c1a182a3ba5a4789783b059d42eaf2b35
labuser <katarzyna.kozak4@o2.pl>
2024-11-29 09:24:27 +0100

    test

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -2,6 +2,9 @@ package eu.chrost.git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello World!");
+        System.out.println("hello World World!");
     }
+
+
 }
