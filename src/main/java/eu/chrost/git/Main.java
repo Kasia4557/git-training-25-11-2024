1c3f493c97cfe8c0ed0a33e238afa0a05293d2bf
labuser <katarzyna.kozak4@o2.pl>
2024-11-29 09:41:44 +0100

    test1

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,9 +1,14 @@
 package eu.chrost.git;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello World!");
-        System.out.println("hello World World!");
+        System.out.println("hello World World!!!");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        scanner.close();
     }
 
 
